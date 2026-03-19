258d25798481a9c67bcf30baccb16aa997c080b8
StefanD3V <stefandimitrijevicweb@gmail.com>
2026-03-19 16:54:17 +0100

    error

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,17 +1,20 @@
-import { createBrowserRouter } from "react-router";
+import { createBrowserRouter } from "react-router-dom";
 import NotFoundPage from "../pages/NotFoundPage";
 import HomePage from "../pages/HomePage";
-import MainLayout from "../layouts/MainLayout.tsx";
+import MainLayout from "../layouts/MainLayout";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    errorElement: <NotFoundPage />,
     children: [
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: "*",
+        element: <NotFoundPage />,
       },
     ],
   },
