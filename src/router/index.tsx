dcebba01e5cf6d0a83d2d7612fab85b1915d1ccf
StefanD3V <stefandimitrijevicweb@gmail.com>
2026-03-19 23:45:00 +0100

    new

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,20 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import NotFoundPage from "../pages/NotFoundPage";
 import HomePage from "../pages/HomePage";
-import MainLayout from "../layouts/MainLayout";
+import MainLayout from "../layouts/MainLayout.tsx";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
+    errorElement: <NotFoundPage />,
     children: [
       {
         index: true,
         element: <HomePage />,
-      },
-      {
-        path: "*",
-        element: <NotFoundPage />,
       },
     ],
   },
