f7693f7e8abe6a6f2e9a26dda1ded40349ba61ee
StefanD3V <stefandimitrijevicweb@gmail.com>
2026-03-19 16:54:54 +0100

    err

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from "react-router";
 import NotFoundPage from "../pages/NotFoundPage";
 import HomePage from "../pages/HomePage";
 import MainLayout from "../layouts/MainLayout";
